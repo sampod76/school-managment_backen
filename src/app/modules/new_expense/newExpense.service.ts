51dc8892bedef201a4a4fcd099f183229b56f7a9
ahmad muas <amaahmadmusa@gmail.com>
2023-08-29 22:11:09 +0600

    some code is added

--- src/app/modules/new_expense/newExpense.service.ts
+++ src/app/modules/new_expense/newExpense.service.ts
@@ -154,7 +154,9 @@ const getYearlyExpensesFromDb = async (): Promise<IExpense[] | null> => {
   return allExpense;
 };
 const getAllNewExpensesFromDb = async (): Promise<IExpense[] | null> => {
-  const allBooks = ExpenseModel.find({});
+  // const allBooks = ExpenseModel.find({});
+  const allBooks = await ExpenseModel.find({}).sort({ createdAt: -1 });
+
   if (!allBooks) {
     throw new ApiError(
       httpStatus.EXPECTATION_FAILED,
